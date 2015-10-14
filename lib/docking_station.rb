25c42a66dda71d15be465ab4054ebc8e7482f8bd
Mateja <mateja@Matejas-MacBook-Pro.local>
2015-10-14 16:30:41 +0100

    adding max 20 to docking station

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,15 +1,21 @@
 require_relative 'bike'
 class DockingStation
-  attr_reader :bike
+
+  def initialize
+    @bikes = Array.new
+  end
+
+  attr_reader :bikes
+
   def release_bike
-    fail "biking station is empty" unless @bike
-    @bike
+    fail "biking station is empty" unless @bikes.count != 0
+    @bikes.pop
     end
 
 
 
   def dock(bike)
-    fail 'station is full' unless !@bike
-    @bike = bike
+    fail 'station is full'  if @bikes.count > 19
+    @bikes.push(bike)
   end
 end
--- spec/describe_dockingstation_spec.rb
+++ spec/describe_dockingstation_spec.rb
@@ -1,16 +1,15 @@
 require 'docking_station'
+
 describe DockingStation do
   it 'responds to release_bike' do
     expect(subject).to respond_to :release_bike
   end
+
   describe '#release_bike' do
     it 'releases a bike' do
     bike = Bike.new
     subject.dock(bike)
     expect(subject.release_bike).to eq bike
-
-  #  expect(bike).to be_working
-
     end
   end
 
@@ -22,7 +21,7 @@ describe DockingStation do
 
   it 'checks if bike has been docked' do
     bike = Bike.new
-    expect(subject.dock(bike)).to eq bike
+    expect(subject.dock(bike)).to eq [bike]
   end
 
     describe '#release_bike' do
@@ -35,16 +34,24 @@ describe DockingStation do
 
      describe ' #dock()' do
          bike = Bike.new
-
          it 'raises an error when docking station is full' do
-           subject.dock(bike)
+           20.times{subject.dock(Bike.new)}
           expect {subject.dock(bike)}.to raise_error("station is full")
          end
      end
 
+     describe '#release_bike' do
+       it 'tests whether release bike removes object from array' do
+         subject.dock(Bike.new)
+         expect {subject.release_bike}.to change {subject.bikes.length}.by(-1)
+       end
+      end
 
-
-
+      describe 'dock()' do
+        it ' tests whether dock bike adds object to array' do
+          expect {subject.dock(Bike.new)}.to change {subject.bikes.length}.by(1)
+        end
+      end
 
 
 end
